ed445ab1a261153771559de2ccf9546856b44561
nadiaf85 <nadia.fiorio@alice.it>
2022-01-13 15:27:58 +0100

    finito

---
+++ js/myscript.js
@@ -0,0 +1,13 @@
+// Stampare a schermo un messaggio all’interno di un h1,
+//  utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+
+let app = new Vue ({
+    el : '#miapp',
+    data : {
+        message : 'Benvenuto su...',
+        image: 'https://cdn-ssl-devio-img.classmethod.jp/wp-content/uploads/2021/08/vuejs-logo.jpg'
+    },
+});
